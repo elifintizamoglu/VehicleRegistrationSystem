e6d80d8ae3ff5d4565992a6e3a0ead5766a04101
elifintizamoglu <elifintizam@gmail.com>
2023-10-08 16:07:02 +0300

    first commit

---
+++ src/test/java/com/elifintizam/VehicleRegistrationSystem/VehicleRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elifintizam.VehicleRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
